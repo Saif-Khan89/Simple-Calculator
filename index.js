9ba7468e77acda74791bbe1f44c59c1636f026f9
Saif khan <saifmazhar89@gmail.com>
2024-04-05 16:35:41 +0500

    Add files via upload
    In this project i have made a simple calculator using typescript , node.js and inquirer .

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter firstNumber", type: "number", name: "firstNumber" },
+    { message: "enter secondNumber", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "divison",],
+    }
+]);
+// conditional statement
+if (answer.operator === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "divison") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("please select a valid operator");
+}
